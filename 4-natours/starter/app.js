33d81683ee37afd24e55382c767c4bebd1700822
weerawit32 <vasthabo@hotmail.com>
2023-03-26 17:11:26 +0700

    error handlering

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppErorr = require('./utils/appError');
+
 const userRouter = require('./routes/userRouter');
 const tourRouter = require('./routes/tourRouter');
 
@@ -145,6 +147,29 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Cant find ${req.originalUrl} on this server`,
+  // });
+  // const err = new Error(`Cant find ${req.originalUrl} on this server`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
+
+  next(new AppErorr(`Cant find ${req.originalUrl} on this server`), 404);
+});
+
+app.use((err, req, res, next) => {
+  console.log('stack', err.stack);
+
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 
 module.exports = app;
 
